86872a59d48728f2c03fa6cfba53b3748d59e7a5
zhangkang <zhangkang@lvmama.com>
2019-07-20 23:38:45 +0800

    jacoco代码覆盖率

--- src/test/java/org/zk/ConsumerTest.java
+++ src/test/java/org/zk/ConsumerTest.java
@@ -10,6 +10,9 @@ import static org.junit.Assert.*;
  */
 public class ConsumerTest {
 
+    //    mvn clean test org.jacoco:jacoco-maven-plugin:0.8.4:prepare-agent install -Dmaven.test.failure.ignore=true
+
+
     @Test
     public void statement() throws Exception {
         Consumer consumer = new Consumer("zk");
@@ -18,18 +21,17 @@ public class ConsumerTest {
         Movie childMovie = new Movie("childMovie", Movie.CHILDRENS);
         consumer.addRental(new Rental(regularMovie, 2));
         consumer.addRental(new Rental(regularMovie, 5));
-        consumer.addRental(new Rental(newReleaseMovie, 2));
+//        consumer.addRental(new Rental(newReleaseMovie, 2));
         consumer.addRental(new Rental(childMovie, 2));
         consumer.addRental(new Rental(childMovie, 5));
-//        System.out.println(consumer.statement());
         Assert.assertEquals("Rental Record for zk\n" +
                 "\tregular movie\t2.0\n" +
                 "\tregular movie\t6.5\n" +
-                "\tnewReleaseMovie\t6.0\n" +
+//                "\tnewReleaseMovie\t6.0\n" +
                 "\tchildMovie\t1.5\n" +
                 "\tchildMovie\t4.5\n" +
-                "Amount owed is 20.5\n" +
-                "You earned 6 frequent renter points", consumer.statement());
+                "Amount owed is 14.5\n" +
+                "You earned 4 frequent renter points", consumer.statement());
     }
 
 }
