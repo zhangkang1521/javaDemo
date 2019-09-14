2981e7d5b3c1f72b04707bda07c6545f5a8c655d
zhangkang <zhangkang@lvmama.com>
2019-09-14 21:42:05 +0800

    refactor: 素数，函数抽取重构

--- src/main/java/org/zk/prime/GeneratePrimes.java
+++ src/main/java/org/zk/prime/GeneratePrimes.java
@@ -2,40 +2,61 @@ package org.zk.prime;
 
 public class GeneratePrimes {
 
+    /** 素数标志，true代表是素数 */
+    private static boolean[] primeFlag;
+    private static int[] result;
+
     public static int[] generatePrimes(int maxValue) {
-        if (maxValue >= 2) {
-            int s = maxValue + 1; // size of array
-            boolean[] f = new boolean[s];
-            int i;
-            for (i = 0; i < s; i++) {
-                f[i] = true;
-            }
-            f[0] = f[1] = false; // 0,1 不是素数
-            int j;
-            for (i = 2; i < Math.sqrt(s) + 1; i++) {
-                if (f[i]) {
-                    for (j = 2 * i; j < s; j += i) {
-                        f[j] = false;
-                    }
-                }
-            }
-
-            int count = 0;
-            for (i = 0; i < s; i++) {
-                if (f[i]) {
-                    count++;
-                }
-            }
-
-            int[] primes = new int[count];
-            for (i = 0, j = 0; i < s; i++) {
-                if (f[i]) {
-                    primes[j++] = i;
-                }
-            }
-            return primes;
-        } else {
+        if (maxValue < 2) {
             return new int[0];
+        }
+        initialize(maxValue);
+        flagNotPrime();
+        putPrimeToResult();
+        return result;
+    }
+
+    private static void initialize(int maxValue) {
+        primeFlag = new boolean[maxValue + 1];
+        // 先初始化未全部是素数，后面用埃氏筛选，将不是素数的打上标记
+        for (int i = 0; i < primeFlag.length; i++) {
+            primeFlag[i] = true;
+        }
+        primeFlag[0] = primeFlag[1] = false; // 0,1 不是素数
+    }
+
+    private static void flagNotPrime() {
+        for (int i = 2; i <= Math.sqrt(primeFlag.length); i++) {
+            if (primeFlag[i]) { // 是素数，将素数的倍数值都置为不是素数
+                flagMultiOf(i);
+            }
+        }
+    }
+
+    private static void flagMultiOf(int i) {
+        for (int j = 2 * i; j < primeFlag.length; j += i) {
+            primeFlag[j] = false;
+        }
+    }
+
+
+
+    private static int countPrime() {
+        int count = 0;
+        for (int i = 0; i < primeFlag.length; i++) {
+            if (primeFlag[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static void putPrimeToResult() {
+        result = new int[countPrime()];
+        for (int i = 0, j = 0; i < primeFlag.length; i++) {
+            if (primeFlag[i]) {
+                result[j++] = i;
+            }
         }
     }
 
