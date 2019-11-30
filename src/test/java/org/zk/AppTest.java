2816dae75620f7e9618968bd82113a46bdd4b5ac
zhangkang <zhangkang@lvmama.com>
2019-11-30 21:58:19 +0800

    javassist 实现动态代理

--- src/test/java/org/zk/AppTest.java
+++ src/test/java/org/zk/AppTest.java
@@ -1,7 +1,12 @@
 package org.zk;
 
 
+import javassist.util.proxy.MethodHandler;
+import javassist.util.proxy.Proxy;
+import javassist.util.proxy.ProxyFactory;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 /**
  * Unit test for simple App.
@@ -9,7 +14,31 @@ import org.junit.Test;
 public class AppTest {
 
     @Test
-    public void test1() {
-        System.out.println("hello");
+    public void test1() throws Exception{
+        ProxyFactory enhancer = new ProxyFactory();
+        enhancer.setSuperclass(UserService.class);
+        Object obj = enhancer.create(null, null);
+        ((Proxy) obj).setHandler(new MethodHandler() {
+            public Object invoke(Object self, Method method, Method proceed, Object[] args) throws Throwable {
+                System.out.println("=========");
+                return proceed.invoke(self, args);
+            }
+        });
+        String str = ((UserService)obj).sayHello("zhangxuan");
+        System.out.println(str);
     }
+
+//    @Test
+//    public void proxyInterface() throws Exception{
+//        ProxyFactory enhancer = new ProxyFactory();
+//        enhancer.setSuperclass(UserDao.class);
+//        Object obj = enhancer.
+//        ((Proxy) obj).setHandler(new MethodHandler() {
+//            public Object invoke(Object self, Method method, Method proceed, Object[] args) throws Throwable {
+//                return "xx";
+//            }
+//        });
+//        String str = ((UserDao)obj).findById(1);
+//        System.out.println(str);
+//    }
 }
