7771aaef38b339e9bee0e6815f69ec88a13126ba
zhangkang <zhangkang@lvmama.com>
2020-05-11 15:27:50 +0800

    jackson json,xml转换

---
+++ src/test/java/org/zk/JacksonTest.java
@@ -0,0 +1,44 @@
+package org.zk;
+
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JacksonTest {
+
+    @Test
+    public void objectToJson() throws Exception {
+        User user = new User();
+        user.setUsername("zk");
+        String result = new ObjectMapper().writeValueAsString(user);
+        System.out.println(result);
+    }
+
+    @Test
+    public void jsonToObject() throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        User user = mapper.readValue("{\"username\":\"zk\"}", User.class);
+        Assert.assertEquals("zk", user.getUsername());
+    }
+
+    @Test
+    public void objectToXml() throws Exception {
+        User user = new User();
+        user.setUsername("zk");
+        String result = new XmlMapper().writeValueAsString(user);
+        System.out.println(result);
+    }
+
+    @Test
+    public void xmlToObject() throws Exception{
+        XmlMapper mapper = new XmlMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        User user = mapper.readValue("<user><username>zk</username></user>", User.class);
+        Assert.assertEquals("zk", user.getUsername());
+    }
+
+}
