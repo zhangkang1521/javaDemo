cc0c9f49408d829b7291b6386b119b83e356e9a1
zhangkang <zhangkang@lvmama.com>
2020-07-26 20:03:58 +0800

    时区问题

--- src/test/java/org/zk/EasyExcelTest.java
+++ src/test/java/org/zk/EasyExcelTest.java
@@ -3,12 +3,18 @@ package org.zk;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import org.apache.poi.util.LocaleUtil;
 import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class EasyExcelTest {
 
 	@Test
 	public void testRead() {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
 		EasyExcel.read("E:/test.xlsx", User.class, new AnalysisEventListener<User>() {
 
 			@Override
@@ -21,5 +27,19 @@ public class EasyExcelTest {
 
 			}
 		}).sheet().doRead();
+	}
+
+	@Test
+	public void testDate() throws Exception {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+
+		Calendar calendar1 = LocaleUtil.getLocaleCalendar();
+		calendar1.set(1900,0, 32982 - 1, 0, 0, 0);
+		Date date1 = calendar1.getTime();
+
+
+		Calendar calendar2 = LocaleUtil.getLocaleCalendar();
+		calendar1.set(1900,0, 43940 - 1, 0, 0, 0);
+		Date date2 = calendar2.getTime();
 	}
 }
--- src/test/java/org/zk/User.java
+++ src/test/java/org/zk/User.java
@@ -5,8 +5,24 @@ import com.alibaba.excel.annotation.ExcelProperty;
 import java.util.Date;
 
 public class User {
-	@ExcelProperty(value = "入职时间", index = 0, format = "yyyy/MM/dd")
+
+//	@ExcelProperty(value = "工号", index = 0)
+//	private String no;
+
+	@ExcelProperty(value = "生日", index = 0, format = "yyyy/MM/dd")
 	private Date birthday;
+
+//	@ExcelProperty(value = "生日", index = 5)
+//	private String birthdayStr;
+
+
+//	public String getNo() {
+//		return no;
+//	}
+//
+//	public void setNo(String no) {
+//		this.no = no;
+//	}
 
 	public Date getBirthday() {
 		return birthday;
