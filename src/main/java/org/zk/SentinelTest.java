147561bcd89b7c696fbfd9a68e86509622f2f3cf
zhangkang <zhangkang@lvmama.com>
2020-11-30 18:30:26 +0800

    哨兵

--- src/main/java/org/zk/SentinelTest.java
+++ src/main/java/org/zk/SentinelTest.java
@@ -1,19 +1,39 @@
 package org.zk;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisSentinelPool;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SentinelTest {
 
-	public static void main(String[] args) {
+	private static Logger log = LoggerFactory.getLogger(SentinelTest.class);
+
+	public static void main(String[] args) throws Exception {
 		Set sentinels = new HashSet();
-		sentinels.add("192.168.127.128:26379");
+		sentinels.add("192.168.127.132:26379");
+		sentinels.add("192.168.127.132:26380");
+		sentinels.add("192.168.127.132:26381");
 		JedisSentinelPool sentinelPool = new JedisSentinelPool("mymaster", sentinels);
-		Jedis jedis = sentinelPool.getResource(); // 返回master节点
-		jedis.set("a", "1123");
-		System.out.println(jedis.get("a"));
+
+		for (int i = 0; i < 10000; i++) {
+			Jedis jedis = null;
+			try {
+				jedis = sentinelPool.getResource(); // 返回master节点
+				jedis.set("a", i + "");
+				log.info("set and get ok :" + jedis.get("a"));
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				e.printStackTrace(); // master重新选举这一段时间会有报错，十几秒
+			} finally {
+				if (jedis != null) {
+					jedis.close();
+				}
+			}
+		}
 	}
 }
