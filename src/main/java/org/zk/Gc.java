cef6ab5a6b3d18f6177c008c7ce4489dfbe629ab
zhangkang <zhangkang@lvmama.com>
2021-04-13 15:07:43 +0800

    垃圾回收器

--- src/main/java/org/zk/Gc.java
+++ src/main/java/org/zk/Gc.java
@@ -5,7 +5,31 @@ import java.lang.management.ManagementFactory;
 import java.util.List;
 
 /**
- * -XX:+PrintGCDetails
+ * https://stackoverflow.com/questions/39929758/ps-marksweep-is-which-garbage-collector/44923227
+ * 串行收集器
+ * -XX:+UseSerialGC
+ * 输出：
+ * Copy 复制（新生代）
+ * MarkSweepCompact 标记-整理（老年代）
+ *
+ * 并行收集器（使用多线程，其他与串行收集器一致）
+ * -XX:+UseParallelGC  -XX:+UseParallelOldGC（不加参数jdk8默认）
+ * 输出：
+ * PS Scavenge（新生代）
+ * PS MarkSweep （老年代）
+ *
+ * CMS收集器（老年代）
+ * -XX:+UseConcMarkSweepGC
+ * 输出：
+ * ParNew （新生代）
+ * ConcurrentMarkSweep （老年代）
+ *
+ * G1收集器，用来代替CMS
+ *-XX:+UseG1GC
+ * 输出：
+ * G1 Young Generation
+ * G1 Old Generation
+ *
  */
 public class Gc {
 
